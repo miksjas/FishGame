15553feacd4800a1c03836649a8b728e38eae34c
miksjas <jasinski.mikolaj@gmail.com>
2023-01-17 23:32:48 +0100

    form fix

--- Project1/MenuForm.Designer.cs
+++ Project1/MenuForm.Designer.cs
@@ -45,10 +45,9 @@ namespace FishGame
             // 
             // playSoloButton
             // 
-            this.playSoloButton.Location = new System.Drawing.Point(471, 224);
-            this.playSoloButton.Margin = new System.Windows.Forms.Padding(7);
+            this.playSoloButton.Location = new System.Drawing.Point(194, 93);
             this.playSoloButton.Name = "playSoloButton";
-            this.playSoloButton.Size = new System.Drawing.Size(204, 65);
+            this.playSoloButton.Size = new System.Drawing.Size(84, 27);
             this.playSoloButton.TabIndex = 0;
             this.playSoloButton.Text = "Play Solo";
             this.playSoloButton.UseVisualStyleBackColor = true;
@@ -57,10 +56,9 @@ namespace FishGame
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(471, 29);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(7);
+            this.pictureBox1.Location = new System.Drawing.Point(194, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(204, 159);
+            this.pictureBox1.Size = new System.Drawing.Size(84, 66);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -76,10 +74,9 @@ namespace FishGame
             "6",
             "7",
             "8"});
-            this.enterSensorAmount.Location = new System.Drawing.Point(471, 470);
-            this.enterSensorAmount.Margin = new System.Windows.Forms.Padding(7);
+            this.enterSensorAmount.Location = new System.Drawing.Point(194, 195);
             this.enterSensorAmount.Name = "enterSensorAmount";
-            this.enterSensorAmount.Size = new System.Drawing.Size(230, 49);
+            this.enterSensorAmount.Size = new System.Drawing.Size(97, 25);
             this.enterSensorAmount.TabIndex = 3;
             // 
             // enterHiddenNeuronAmount
@@ -87,7 +84,6 @@ namespace FishGame
             this.enterHiddenNeuronAmount.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.enterHiddenNeuronAmount.FormattingEnabled = true;
             this.enterHiddenNeuronAmount.Items.AddRange(new object[] {
-            "1",
             "2",
             "3",
             "4",
@@ -95,48 +91,43 @@ namespace FishGame
             "6",
             "7",
             "8"});
-            this.enterHiddenNeuronAmount.Location = new System.Drawing.Point(850, 470);
-            this.enterHiddenNeuronAmount.Margin = new System.Windows.Forms.Padding(7);
+            this.enterHiddenNeuronAmount.Location = new System.Drawing.Point(350, 195);
             this.enterHiddenNeuronAmount.Name = "enterHiddenNeuronAmount";
-            this.enterHiddenNeuronAmount.Size = new System.Drawing.Size(232, 49);
+            this.enterHiddenNeuronAmount.Size = new System.Drawing.Size(98, 25);
             this.enterHiddenNeuronAmount.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(117, 422);
-            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Location = new System.Drawing.Point(48, 175);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 41);
+            this.label1.Size = new System.Drawing.Size(79, 17);
             this.label1.TabIndex = 5;
             this.label1.Text = "Fish Amount";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(471, 422);
-            this.label2.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label2.Location = new System.Drawing.Point(194, 175);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(223, 41);
+            this.label2.Size = new System.Drawing.Size(97, 17);
             this.label2.TabIndex = 6;
             this.label2.Text = "Sensor Amount";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(797, 422);
-            this.label3.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label3.Location = new System.Drawing.Point(328, 175);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(338, 41);
+            this.label3.Size = new System.Drawing.Size(147, 17);
             this.label3.TabIndex = 7;
             this.label3.Text = "Hidden Neuron Amount";
             // 
             // runSimulationButton
             // 
-            this.runSimulationButton.Location = new System.Drawing.Point(447, 569);
-            this.runSimulationButton.Margin = new System.Windows.Forms.Padding(7);
+            this.runSimulationButton.Location = new System.Drawing.Point(184, 236);
             this.runSimulationButton.Name = "runSimulationButton";
-            this.runSimulationButton.Size = new System.Drawing.Size(289, 55);
+            this.runSimulationButton.Size = new System.Drawing.Size(119, 23);
             this.runSimulationButton.TabIndex = 8;
             this.runSimulationButton.Text = "Run simulation";
             this.runSimulationButton.UseVisualStyleBackColor = true;
@@ -153,17 +144,16 @@ namespace FishGame
             "40",
             "50",
             "60"});
-            this.enterFishAmount.Location = new System.Drawing.Point(95, 470);
-            this.enterFishAmount.Margin = new System.Windows.Forms.Padding(7);
+            this.enterFishAmount.Location = new System.Drawing.Point(39, 195);
             this.enterFishAmount.Name = "enterFishAmount";
-            this.enterFishAmount.Size = new System.Drawing.Size(230, 49);
+            this.enterFishAmount.Size = new System.Drawing.Size(97, 25);
             this.enterFishAmount.TabIndex = 9;
             // 
             // MenuForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1183, 654);
+            this.ClientSize = new System.Drawing.Size(487, 271);
             this.Controls.Add(this.enterFishAmount);
             this.Controls.Add(this.runSimulationButton);
             this.Controls.Add(this.label3);
@@ -174,7 +164,6 @@ namespace FishGame
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.playSoloButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(7);
             this.MaximizeBox = false;
             this.Name = "MenuForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
